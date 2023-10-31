b6e0f632e459f1f4b7b45ac697819d27c8f7a7b2
kgagracz <krystian.gagracz@gmail.com>
2023-10-31 21:21:10 +0100

    Initialized eslint

--- App.tsx
+++ App.tsx
@@ -1,22 +1,27 @@
-import {HomeScreen} from "./screens/Home/Home.Screen";
 import * as Font from 'expo-font';
-import {useState} from "react";
-import AppLoading from 'expo-app-loading'
+import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import { Home } from './screens/Home/Home';
 
-const getFonts = () =>
-    Font.loadAsync({
-      'poppins': require('./assets/fonts/Poppins-Regular.ttf'),
-      'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf')
-    })
+const getFonts = () => Font.loadAsync({
+  poppins: require('./assets/fonts/Poppins-Regular.ttf'),
+  'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
+});
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false)
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if (!fontsLoaded) {
-    return <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} onError={console.warn}/>
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
   }
 
   return (
-    <HomeScreen />
+    <Home />
   );
 }
---
+++ screens/Home/Home.tsx
@@ -0,0 +1,34 @@
+import { useTranslation } from 'react-i18next';
+import { Image, StyleSheet, View } from 'react-native';
+import { NormalText } from '../../components/atoms/NormalText';
+import { BoldText } from '../../components/atoms/BoldText';
+import '../../i18n';
+
+export function Home() {
+  const { t } = useTranslation();
+
+  return (
+    <View>
+      <NormalText style={styles.helloText} text={t('welcomeMessage')} />
+      <BoldText style={styles.currentCityName} text="Kraków" />
+      <Image source={require('../../assets/images/cracow.jpg')} style={styles.currentCityImage} />
+      {/* <StatusBar style={"auto"} /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  helloText: {
+    fontSize: 16,
+    marginTop: 70,
+    textAlign: 'center',
+  },
+  currentCityName: {
+    fontSize: 28,
+    textAlign: 'center',
+  },
+  currentCityImage: {
+    height: 400,
+    objectFit: 'cover',
+  },
+});
