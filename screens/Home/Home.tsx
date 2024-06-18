8b3bff167e1420fd5a3e9a780ccec88945fce96c
kgagracz <krystian.gagracz@gmail.com>
2024-06-18 20:24:40 +0200

    feat: Added map

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,34 +1,14 @@
 import { useTranslation } from 'react-i18next'
-import { Image, StyleSheet, View } from 'react-native'
-import { NormalText } from '../../components/atoms/NormalText'
-import { BoldText } from '../../components/atoms/BoldText'
+import { View } from 'react-native'
 import '../../i18n'
+import Map from '../../components/organisms/Map'
 
 export function Home() {
   const { t } = useTranslation()
 
   return (
     <View>
-      <NormalText style={styles.helloText} text={t('welcomeMessage')} />
-      <BoldText style={styles.currentCityName} text="Kraków" />
-      <Image source={require('../../assets/images/cracow.jpg')} style={styles.currentCityImage} />
-      {/* <StatusBar style={"auto"} /> */}
+      <Map fullScreen />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  helloText: {
-    fontSize: 16,
-    marginTop: 70,
-    textAlign: 'center',
-  },
-  currentCityName: {
-    fontSize: 28,
-    textAlign: 'center',
-  },
-  currentCityImage: {
-    height: 400,
-    objectFit: 'cover',
-  },
-})
