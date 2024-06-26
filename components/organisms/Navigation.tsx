d9c49820a5b859d4b8ed23213c68313645b40f17
kgagracz <krystian.gagracz@gmail.com>
2024-06-26 21:00:17 +0200

    feat: Nav structure

--- components/organisms/Navigation.tsx
+++ components/organisms/Navigation.tsx
@@ -1,18 +1,8 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { useTranslation } from 'react-i18next'
-import Icon from '@expo/vector-icons/MaterialIcons'
-import { Home } from '../../screens/Home/Home'
+import { NavigationContainer } from '@react-navigation/native'
+import { TabNavigation } from './Navigation/TabNavigation'
 
-const Tab = createBottomTabNavigator()
-export const Navigation = () => {
-  const { t } = useTranslation()
-
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name={t('menuMap')} component={Home} options={{ tabBarIcon: () => <Icon name="location-on" size={25} /> }} />
-      <Tab.Screen name={t('menuSaved')} component={Home} options={{ tabBarIcon: () => <Icon name="folder-special" size={25} /> }} />
-      <Tab.Screen name={t('menuVisited')} component={Home} options={{ tabBarIcon: () => <Icon name="check-circle-outline" size={25} /> }} />
-      <Tab.Screen name={t('menuSettings')} component={Home} options={{ tabBarIcon: () => <Icon name="settings" size={25} /> }} />
-    </Tab.Navigator>
-  )
-}
+export const Navigation = () => (
+  <NavigationContainer>
+    <TabNavigation />
+  </NavigationContainer>
+)
