4a4293f2a23c3f42ab89c7b69a4649ec694c24bf
kgagracz <krystian.gagracz@gmail.com>
2024-07-08 22:04:37 +0200

    feat: Async storage handling and basic communication functions

---
+++ services/asyncStorage.ts
@@ -0,0 +1,42 @@
+import AsyncStorage from '@react-native-community/async-storage'
+import { VISITED_ATTRACTION_API_URL } from '../api/visitedAttractions.api'
+import { SAVED_ATTRACTIONS_API_URL } from '../api/savedAttractions.api'
+import savedAttractionsMock from '../mocks/savedAttractions.json'
+
+export const initAsyncStorage = async () => {
+  await AsyncStorage.clear()
+  await AsyncStorage.setItem(VISITED_ATTRACTION_API_URL, '[]')
+  await AsyncStorage.setItem(SAVED_ATTRACTIONS_API_URL, JSON.stringify(savedAttractionsMock))
+}
+
+export const getFromAsyncStorage = async (key: string) => {
+  try {
+    return await AsyncStorage.getItem(key)
+  } catch (error) {
+    // Error saving data
+  }
+}
+
+export const addItemToAsyncStorageArray = async (key: string, item: unknown) => {
+  const existingValue = await AsyncStorage.getItem(key)
+  if (!existingValue) { return }
+  const existingArray = JSON.parse(existingValue)
+  if (!Array.isArray(existingArray)) { return }
+  existingArray.push(item)
+  await AsyncStorage.setItem(key, JSON.stringify(existingArray))
+}
+
+export const mergeItemToAsyncStorage = async (key: string, value: unknown) => {
+  await AsyncStorage.mergeItem(key, JSON.stringify(value))
+}
+
+export const saveToAsyncStorage = async (key: string, value: unknown) => {
+  try {
+    const stringValue = JSON.stringify(value)
+    await AsyncStorage.setItem(key, stringValue)
+  } catch (error) {
+    // Error saving data
+  }
+}
+
+export const removeFromAsyncStorage = async (key: string) => AsyncStorage.removeItem(key)
