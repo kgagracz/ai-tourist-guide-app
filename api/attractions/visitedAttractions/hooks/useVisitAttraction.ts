d6b93bd2223dad1da780b29bb492539199958809
kgagracz <krystian.gagracz@gmail.com>
2024-11-05 23:58:30 +0100

    feat: Added visiting and saving attraction errors handling

--- api/attractions/visitedAttractions/hooks/useVisitAttraction.ts
+++ api/attractions/visitedAttractions/hooks/useVisitAttraction.ts
@@ -1,7 +1,12 @@
 import { useMutation } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { AxiosError } from 'axios'
 import { visitAttraction } from '../services/visitedAttractionsService'
 import { VISIT_ATTRACTION } from '../queryKeys'
 import { MutationCallbacksModel } from '../../../models/QueryCallbacksModel'
+import useToast from '../../../../hooks/useToast'
+import { SaveAttractionBody } from '../../savedAttractions/models/saveAttractionModel'
 
 interface UseSaveAttractionOptions extends MutationCallbacksModel {
 
@@ -9,11 +14,22 @@ interface UseSaveAttractionOptions extends MutationCallbacksModel {
 
 export const useVisitAttraction = (options: UseSaveAttractionOptions) => {
   const { onSuccess, onError } = options
+  const { t } = useTranslation()
+  const { showToast } = useToast()
 
-  return useMutation({
+  const query = useMutation<unknown, AxiosError, SaveAttractionBody, unknown>({
     mutationFn: visitAttraction,
     mutationKey: [VISIT_ATTRACTION],
     onSuccess,
     onError,
   })
+  const { error } = query
+
+  useEffect(() => {
+    if (!error) return
+
+    showToast(t('VISITING_ATTRACTION_FAILED'))
+  }, [error, t])
+
+  return query
 }
