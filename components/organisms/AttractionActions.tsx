5b81ea8fd58a395c9d17ba44dc6f314201d50cd7
kgagracz <krystian.gagracz@gmail.com>
2024-11-24 16:25:09 +0100

    refactor: wrap onPress functions in useCallback

--- components/organisms/AttractionActions.tsx
+++ components/organisms/AttractionActions.tsx
@@ -1,46 +1,51 @@
 import { Linking, StyleSheet, View } from 'react-native'
 import Icon from '@expo/vector-icons/MaterialIcons'
 import { useTheme } from '@react-navigation/native'
-import { useQueryClient } from '@tanstack/react-query'
+import { useCallback } from 'react'
 import { Button } from '../atoms/Button'
 import { ThemeType } from './SchemeContext/SchemeProvider'
 import { Attraction } from '../../models/Attraction'
-import useToast from '../../hooks/useToast'
 import { useSaveAttraction } from '../../hooks/queryHooks/attractions/useSaveAttraction'
 import { useVisitAttraction } from '../../hooks/queryHooks/attractions/useVisitAttraction'
 
 type AttractionActionsProps = {
     attraction: Attraction
 }
-const USER_ID = 1
+
 export const AttractionActions = ({ attraction }: AttractionActionsProps) => {
   const theme = useTheme()
   const styles = makeStyles(theme)
-  const queryClient = useQueryClient()
-  const { showToast } = useToast()
-
   const { mutate: saveAttraction } = useSaveAttraction()
   const { mutate: addToVisited } = useVisitAttraction()
+
   const onAddToVisitedClick = async () => {
     if (!attraction || attraction.id === -1) { return }
-    await addToVisited({ attractionOverpassId: attraction.id, userId: USER_ID })
+    await addToVisited({ attractionOverpassId: attraction.id })
   }
-  const onAddToSavedClick = async () => {
-    if (!attraction || attraction.id === -1) { return }
-    saveAttraction({ attractionOverpassId: attraction.id, userId: USER_ID })
-  }
-  const onNavigateClick = async () => {
-    if (!attraction) { return }
+  const onAddToSavedClick = useCallback(async () => {
+    if (!attraction || attraction.id === -1) {
+      return
+    }
+    saveAttraction({ attractionOverpassId: attraction.id })
+  }, [attraction, saveAttraction])
+
+  const onNavigateClick = useCallback(async () => {
+    if (!attraction) {
+      return
+    }
     await Linking.openURL(
       `https://www.google.com/maps/dir/?api=1&destination=${attraction.name} ${attraction.city}`,
     )
-  }
-  const onSearchInternetClick = async () => {
-    if (!attraction) { return }
+  }, [attraction])
+
+  const onSearchInternetClick = useCallback(async () => {
+    if (!attraction) {
+      return
+    }
     await Linking.openURL(
       `https://www.google.com/search?q=${attraction.name} ${attraction.city}`,
     )
-  }
+  }, [attraction])
 
   return (
     <View style={styles.container}>
