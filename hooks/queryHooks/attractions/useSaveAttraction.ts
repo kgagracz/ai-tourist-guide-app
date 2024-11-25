2cd0ad1d587d9a76694320a4e0bafd7d52d2f7db
kgagracz <krystian.gagracz@gmail.com>
2024-11-25 21:07:34 +0100

    feat: refetch get attraction details query after adding attraction to saved or visited

--- hooks/queryHooks/attractions/useSaveAttraction.ts
+++ hooks/queryHooks/attractions/useSaveAttraction.ts
@@ -4,6 +4,7 @@ import { useQueryClient } from '@tanstack/react-query'
 import { useSaveAttractionQuery } from '../../../api/attractions/savedAttractions/hooks/useSaveAttractionQuery'
 import useToast from '../../useToast'
 import { GET_SAVED_ATTRACTIONS } from '../../../api/attractions/savedAttractions/queryKeys'
+import { GET_ATTRACTION_INTRO } from '../../../api/attractions/queryKeys'
 
 export const useSaveAttraction = () => {
   const { t } = useTranslation()
@@ -16,6 +17,7 @@ export const useSaveAttraction = () => {
 
   const onSuccess = useCallback(async () => {
     queryClient.invalidateQueries({ queryKey: [GET_SAVED_ATTRACTIONS] })
+    queryClient.invalidateQueries({ queryKey: [GET_ATTRACTION_INTRO] })
     showToast('Dodano atrakcję do zapisanych.')
   }, [showToast, queryClient])
 
--- hooks/queryHooks/attractions/useVisitAttraction.ts
+++ hooks/queryHooks/attractions/useVisitAttraction.ts
@@ -4,6 +4,7 @@ import { useQueryClient } from '@tanstack/react-query'
 import { useVisitAttractionQuery } from '../../../api/attractions/visitedAttractions/hooks/useVisitAttractionQuery'
 import useToast from '../../useToast'
 import { GET_VISITED_ATTRACTIONS } from '../../../api/attractions/visitedAttractions/queryKeys'
+import { GET_ATTRACTION_INTRO } from '../../../api/attractions/queryKeys'
 
 export const useVisitAttraction = () => {
   const { t } = useTranslation()
@@ -16,7 +17,8 @@ export const useVisitAttraction = () => {
 
   const onSuccess = useCallback(async () => {
     showToast('Dodano atrakcję do odwiedzonych.')
-    await queryClient.invalidateQueries({ queryKey: [GET_VISITED_ATTRACTIONS] })
+    queryClient.invalidateQueries({ queryKey: [GET_VISITED_ATTRACTIONS] })
+    queryClient.invalidateQueries({ queryKey: [GET_ATTRACTION_INTRO] })
   }, [showToast, queryClient])
 
   return useVisitAttractionQuery({
